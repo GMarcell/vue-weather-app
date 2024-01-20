7267576852a501beecbd5e72d1669e3c01502fe9
Grand Marcell <grand1310marcell@gmail.com>
2024-01-21 01:23:56 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
+<script setup>
+import SearchInput from './components/SearchInput.vue';
+
+
+</script>
 <template>
-  <main>Home</main>
+  <main>
+    <!-- Date -->
+    <div class="text-center mb-6 ">
+      {{
+        new Date().toLocaleDateString('en-us', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+      }}
+    </div>
+
+    <!-- Search -->
+    <div>
+      <SearchInput/>
+    </div>
+  </main>
 </template>
